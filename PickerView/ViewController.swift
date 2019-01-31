3333b938bbd0880eac35e9badaef243de0b77fa7
R. Kukuh <rkukuh@gmail.com>
2019-01-31 09:38:33 +0700

    Refactor and debugging

--- PickerView/ViewController.swift
+++ PickerView/ViewController.swift
@@ -15,28 +15,31 @@ class ViewController: UIViewController,
     
     let cities = ["Jakarta", "Surabaya", "Bandung", "Sidoarjo"]
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int,
+                    inComponent component: Int) {
+        print("User selected row #\(row) of the \(component) component")
+    }
+    
     // MARK: PickerView's delegate stubs
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int,
                     forComponent component: Int) -> String? {
-        
         return cities[row]
     }
     
     // MARK: PickerView's protocol stubs
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
+        return 2
     }
     
     func pickerView(_ pickerView: UIPickerView,
                     numberOfRowsInComponent component: Int) -> Int {
-        
         return cities.count
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
     }
 }
 
