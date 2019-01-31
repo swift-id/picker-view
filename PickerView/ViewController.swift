59142c08691e491e66032307498ede156cf6d28a
R. Kukuh <rkukuh@gmail.com>
2019-01-31 09:26:43 +0700

    Define delegate and data source

--- PickerView/ViewController.swift
+++ PickerView/ViewController.swift
@@ -8,9 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,
+                      UIPickerViewDelegate, UIPickerViewDataSource {
+    
     @IBOutlet weak var citiesPickerView: UIPickerView!
+    
+    let cities = ["Jakarta", "Surabaya", "Bandung", "Sidoarjo"]
+    
+    // MARK: PickerView's delegate stubs
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int,
+                    forComponent component: Int) -> String? {
+        return "Malang"
+    }
+    
+    // MARK: PickerView's protocol stubs
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView,
+                    numberOfRowsInComponent component: Int) -> Int {
+        return 3
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
